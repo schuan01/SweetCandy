08dcec211715a6366ba9e3e2645be7aa3ba997f9
schuan <schuanv@gmail.com>
2016-12-13 12:06:53 -0300

    Cambios BD

--- server/main.js
+++ server/main.js
@@ -4,12 +4,12 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 app.set('port', (process.env.PORT || 5000));
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-var server_bd_port = 3306
-var server_bd = 'us-cdbr-iron-east-04.cleardb.net'
-var server_user 'b16fa9963b6b69'
-var server_pwd = '5080d557'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_bd_port = 3306;
+var server_bd = 'us-cdbr-iron-east-04.cleardb.net';
+var server_user = 'b16fa9963b6b69';
+var server_pwd = '5080d557';
 
 
 var mysql      = require('mysql');
