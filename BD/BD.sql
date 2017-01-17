183adfd12486263bee4e7d0600139e1343621087
schuan <schuanv@gmail.com>
2017-01-17 17:53:39 -0300

    Fotos y editar.

--- BD/BD.sql
+++ BD/BD.sql
@@ -9,6 +9,26 @@ edad INT,
 costo FLOAT,
 latitud DOUBLE,
 longitud DOUBLE,
+urlFoto VARCHAR(200),
 email VARCHAR(50) NOT NULL, /*HAY QUE HACERLO UNIQUE*/
 password VARCHAR(200) NOT NULL
-)
+);
+
+CREATE TABLE Transaccion (
+id INT AUTO_INCREMENT PRIMARY KEY,
+empleadoTransaccion INT NOT NULL,
+clienteTransaccion INT NOT NULL,
+fechaInicioTransaccion DATETIME NOT NULL,
+fechaFinTransaccion DATETIME,
+isActiva BIT,
+totalTransaccion DOUBLE,
+FOREIGN KEY (empleadoTransaccion) REFERENCES Empleado(id)
+);
+
+CREATE TABLE FotosUsuario(
+id INT AUTO_INCREMENT PRIMARY KEY,
+empleadoFoto INT NOT NULL,
+urlFoto VARCHAR(200) NOT NULL,
+FOREIGN KEY (empleadoFoto) REFERENCES Empleado(id));
+
+select * from empleado
--- server/main.js
+++ server/main.js
@@ -262,13 +262,57 @@ socket.on('editarusuario', function(data) {
              if (err1) throw err1;
                connection.release();
                console.log("Usuario: " + data.id + " modificado correctamente");
-               modificado = true;  
+               modificado = true;
+                 
+               socket.emit('usuarioeditado', modificado);//Devolvemos el resultado   
 
             });
       });
 
-  socket.emit('usuarioeditado', modificado);//Devolvemos el resultado   
+
 });
+
+//GUARDAR FOTOS
+socket.on('guardarfoto', function(data) 
+{
+    var urlFoto = data.urlFoto;
+    var usu = data.id;
+    pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query('INSERT INTO FotosUsuario SET empleadoFoto = ?,urlFoto = ?', [usu,urlFoto], function(err1, result) {
+                    if (err1) throw err1;
+                    connection.release();
+                    idTr = result.insertId;//Le ponemos el ID
+                    console.log("Foto agregada con ID: " + idTr);  
+            });
+      });
+  
+
+});
+
+//OBTIENE LAS FOTOS
+socket.on('getfotosusuario', function(data) 
+{
+   pool.getConnection(function(err, connection) {
+        if (err) throw err;
+        connection.query('SELECT * FROM FotosUsuario WHERE empleadoFoto = ?',[data.id], function(error, results, fields) {
+          if (error) throw error;
+          connection.release();
+          var fotos = [];
+          for (var i in results) 
+          {
+            fotos.push(results[i]);
+          }
+          console.log("Fotos obtenidas del Usuario " + data.id + " : " + fotos.length);
+          socket.emit('fotosobtenidas', fotos);     
+          
+        });
+    });
+  
+
+});
+
+
 
 //OBTIENE LOS EMPLEADOS CONECTADOS
 socket.on('conectados', function(data) {   
